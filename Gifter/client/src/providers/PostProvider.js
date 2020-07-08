4924ab44651395fe0425da1c8f403bcda5efe01f
Jayson Rice <jayson.rice.95@gmail.com>
2020-07-08 17:19:23 -0400

    Added more front end firebase code.

--- Gifter/client/src/providers/PostProvider.js
+++ Gifter/client/src/providers/PostProvider.js
@@ -1,25 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useContext, createContext } from "react";
+import { UserProfileContext } from "./UserProfileProvider";
 
 export const PostContext = React.createContext();
 
 export const PostProvider = (props) => {
+    const apiUrl = "/api/quote";
+    const { getToken } = useContext(UserProfileContext);
+
     const [posts, setPosts] = useState([]);
 
-    const getAllPosts = () => {
-        return fetch("/api/post")
-            .then((res) => res.json())
-            .then(setPosts);
-    };
+    const getAllPosts = () =>
+        getToken().then((token) =>
+            fetch(apiUrl, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(resp => resp.json())
+                .then(setPosts));
 
-    const addPost = (post) => {
-        return fetch("/api/post", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(post),
-        });
-    };
+
+    const addPost = (post) =>
+        getToken().then((token) =>
+            fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(post)
+            }).then(resp => {
+                if (resp.ok) {
+                    return resp.json();
+                }
+                throw new Error("Unauthorized");
+            }));
 
     const getPost = (id) => {
         return fetch(`/api/post/${id}`)
